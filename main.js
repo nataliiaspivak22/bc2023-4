64432bd4ee1ea88fa5011efe393be40f5ee92b17
nataliiaspivak22 <nataliiaspivak@lnu.edu.ua>
2023-10-29 15:37:45 +0200

    Fix: тепер сервер повертає дані і для одного елемента

--- main.js
+++ main.js
@@ -18,29 +18,31 @@ const server = http.createServer((req, res) => {
     const parser = new xml.XMLParser(options);
     const obj = parser.parse(xmlData, options);
 
-    if (obj && obj.indicators && Array.isArray(obj.indicators.basindbank)) {
-      const data = obj.indicators.basindbank;
-      const sortedData = data
-        .filter((item) => item.parent === 'BS3_BanksLiab')
-        .map((item) => ({
-          txten: item.txten,
-          value: item.value,
-        }));
+    if (!obj || !obj.indicators || !obj.indicators.basindbank) {
+      throw new Error('В XML немає об\'єктів indicators.basindbank');
+    }
+    const data = obj.indicators.basindbank;
+    const dataArray = Array.isArray(data) ? data : [data];
+    const sortedData = dataArray
+      .filter((item) => item.parent === 'BS3_BanksLiab')
+      .map((item) => ({
+        txten: item.txten,
+        value: item.value,
+      }));
 
-      const newObj = {
-        data: {
-          indicators: sortedData,
-        },
-      };
+    const newObj = {
+      data: {
+        indicators: sortedData,
+      },
+    };
 
-      const builder = new xml.XMLBuilder();
-      const xmlStr = builder.build(newObj);
+    const builder = new xml.XMLBuilder();
+    const xmlStr = builder.build(newObj);
 
       res.writeHead(200, { 'Content-Type': 'application/xml' });
       res.end(xmlStr);
-
     }
-  } catch (error) {
+   catch (error) {
     res.writeHead(500, { 'Content-Type': 'text/plain' });
     res.end('Помилка: ' + error.message);
   }
